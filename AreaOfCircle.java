ae5411db9c493b54e4a3a30093de70085bdb49a2
abbasjafari319 <abbasjafari319@gmail.com>
2025-09-23 09:57:16 +0430

    Update AreaOfCircle.java

--- AreaOfCircle.java
+++ AreaOfCircle.java
@@ -1,21 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package areaofcircle;
 
-/**
- *
- * @author AJ
- */
+
 public class AreaOfCircle {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input=new Scanner(System.in);
+        System.out.println("plz enter the radius");
+        double radius= input.nextDouble();
+        if(radius>=0)
+        System.out.println("the area is "+(Math.PI*radius*radius));
     }
     
 }
+
